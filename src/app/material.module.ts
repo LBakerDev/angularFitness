dd8a23b77e95f85e489003b244dcf39cc4dbf15b
London <london@Dev2-Macbook-Air.local>
2018-04-19 11:35:59 -0400

    tslint fixes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from  '@angular/core';
-import { 
+import { NgModule } from '@angular/core';
+import {
     MatButtonModule,
-    MatIconModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatCheckboxModule   
+    MatCheckboxModule
 } from '@angular/material';
 
 
-//decorator to turn class into decorator
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+    imports: [MatButtonModule, MatIconModule,
+        MatFormFieldModule, MatInputModule,
+        MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+    exports: [MatButtonModule, MatIconModule,
+        MatFormFieldModule, MatInputModule,
+        MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
-export class MaterialModule {}
+
+export class MaterialModule { }
